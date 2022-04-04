4d1a88c4e686210eea82d0ae6aaba3053c0f95f6
Laumania <mads@laumania.net>
2022-04-04 23:02:47 +0200

    Disabled unused ModTools menu items

--- FM Mod Tools/Scripts/Editor/ModToolsMenu.cs
+++ FM Mod Tools/Scripts/Editor/ModToolsMenu.cs
@@ -22,31 +22,19 @@ namespace FM_Mod_Tools.Generated
             UMod.BuildEngine.ModToolsUtil.ShowToolsWindow(typeof(UMod.Exporter.CreateModWindow));
         }
         
-        [UnityEditor.MenuItem("Mod Tools/Create/Prefab Node", priority=1)]
-        internal static void Menu_Create_Prefab_Node()
-        {
-            UMod.BuildEngine.ModToolsUtil.CreatePrefabNode();
-        }
-        
-        [UnityEditor.MenuItem("Mod Tools/Create/Material Node", priority=2)]
-        internal static void Menu_Create_Material_Node()
-        {
-            UMod.BuildEngine.ModToolsUtil.CreateMaterialNode();
-        }
-        
-        [UnityEditor.MenuItem("Mod Tools/Exporter", priority=23)]
+        [UnityEditor.MenuItem("Mod Tools/Exporter", priority=21)]
         internal static void Menu_Exporter()
         {
             UMod.BuildEngine.ModToolsUtil.ShowToolsWindow(typeof(UMod.Exporter.ExporterWindow));
         }
         
-        [UnityEditor.MenuItem("Mod Tools/Export Settings", priority=44)]
+        [UnityEditor.MenuItem("Mod Tools/Export Settings", priority=42)]
         internal static void Menu_Export_Settings()
         {
             UMod.BuildEngine.ModToolsUtil.ShowToolsWindow(typeof(UMod.Exporter.SettingsWindow));
         }
         
-        [UnityEditor.MenuItem("Mod Tools/Build Mod %#b", priority=45)]
+        [UnityEditor.MenuItem("Mod Tools/Build Mod %#b", priority=43)]
         internal static void Menu_Build_Mod___b()
         {
             UMod.ModTools.Export.ExportSettings settings = UMod.ModTools.Export.ExportSettings.Active.Load();
@@ -57,34 +45,22 @@ namespace FM_Mod_Tools.Generated
             UMod.BuildEngine.ModToolsUtil.StartBuild(settings);
         }
         
-        [UnityEditor.MenuItem("Mod Tools/Build and Run %#r", priority=46)]
-        internal static void Menu_Build_and_Run___r()
-        {
-            UMod.BuildEngine.ModToolsUtil.CreateNewMod();
-        }
-        
-        [UnityEditor.MenuItem("Mod Tools/Help", priority=67)]
+        [UnityEditor.MenuItem("Mod Tools/Help", priority=64)]
         internal static void Menu_Help()
         {
             UMod.BuildEngine.ModToolsUtil.ShowToolsWindow(typeof(UMod.Exporter.HelpWindow));
         }
         
-        [UnityEditor.MenuItem("Mod Tools/About", priority=68)]
+        [UnityEditor.MenuItem("Mod Tools/About", priority=65)]
         internal static void Menu_About()
         {
             UMod.BuildEngine.ModToolsUtil.ShowToolsWindow(typeof(UMod.Exporter.AboutWindow));
         }
         
-        [UnityEditor.MenuItem("Mod Tools/Referencing/Rebuild Reference Cache", priority=89)]
+        [UnityEditor.MenuItem("Mod Tools/Referencing/Rebuild Reference Cache", priority=86)]
         internal static void Menu_Referencing_Rebuild_Reference_Cache()
         {
             UMod.BuildEngine.ModToolsUtil.RebuildReferenceCache();
-        }
-        
-        [UnityEditor.MenuItem("Mod Tools/Game Assets", priority=110)]
-        internal static void Menu_Game_Assets()
-        {
-            UMod.BuildEngine.ModToolsUtil.ShowToolsWindow(typeof(UMod.Exporter.GameAssetsWindow));
         }
     }
 }
