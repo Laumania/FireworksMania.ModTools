aeeda43430ec6777ea55fdaecd7fc87736396489
Laumania <mads@laumania.net>
2024-10-12 18:24:48 +0200

    Added FMNetworkVariables for testing

---
+++ FireworksMania/Scripts/Core/Netcode/FMNetworkVariableInteger.cs
@@ -0,0 +1,15 @@
+﻿using Unity.Collections;
+using Unity.Netcode;
+
+namespace FireworksMania.Core.Netcode
+{
+    /// <summary>
+    /// TEMP SOLUTION: Test for using NetworkVariables from Mods. Use this with care as it can break any moment as it's not the way it should be done, but we have to try it as NetCode doesn't support this officially yet.
+    /// </summary>
+    public class FMNetworkVariableInteger : NetworkBehaviour
+    {
+        private NetworkVariable<int> _networkVariable = new NetworkVariable<int>(0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+
+        public NetworkVariable<int> Variable => _networkVariable;
+    }
+}
