5347aa4a80252ff9eba71bd5b958dfb4d82f2dd2
Mads Laumann <mads@laumania.net>
2025-04-16 17:58:14 +0200

    v2025.4.2 (#40)
    * Upgraded to Unity 6
    
    * Added first draft of v2025.3.1
    
    * Upgraded to new UMod version
    
    * Upgraded to uMod 2.9.9p6 for fixing issues with references
    
    * Upgraded with uMod patch to fix ScriptableObject references
    
    * Upgraded package to 2025.3.3 and updated changelog
    
    * Upgraded to 2025.4.1
    
    * Upgraded to 2025.4.2

--- FireworksMania/Scripts/Core/Common/ClientNetworkRigidbody.cs
+++ FireworksMania/Scripts/Core/Common/ClientNetworkRigidbody.cs
@@ -1,4 +1,5 @@
-﻿using Unity.Netcode;
+﻿using FireworksMania.Core.Utilities;
+using Unity.Netcode;
 using Unity.Netcode.Components;
 using UnityEngine;
 
@@ -9,46 +10,23 @@ namespace FireworksMania.Core.Common
     [DisallowMultipleComponent]
     public class ClientNetworkRigidbody : NetworkRigidbody
     {
-        /// <summary>
-        /// Determines if we are server (true) or owner (false) authoritative
-        /// </summary>
-        private bool _isServerAuthoritative;
-
-        private Rigidbody _rigidbody;
         private NetworkTransform _networkTransform;
         private RigidbodyInterpolation _originalInterpolation;
-        private bool _originalIsKinematic;
 
         // Used to cache the authority state of this Rigidbody during the last frame
         private bool _isAuthority;
 
-        private void Awake()
+        protected override void Awake()
         {
-            _networkTransform      = GetComponent<NetworkTransform>();
-            _isServerAuthoritative = _networkTransform.IsServerAuthoritative();
+            base.Awake();
 
-            SetupRigidBody();
+            AutoUpdateKinematicState      = false;
+            _networkTransform             = GetComponent<NetworkTransform>();
+            _originalInterpolation        = this.Rigidbody.interpolation;
+            Rigidbody.interpolation       = _isAuthority ? _originalInterpolation : (_networkTransform.Interpolate ? RigidbodyInterpolation.None : _originalInterpolation);
+            Rigidbody.isKinematic         = true;
         }
 
-        /// <summary>
-        /// If the current <see cref="NetworkTransform"/> has authority,
-        /// then use the <see cref="RigidBody"/> interpolation strategy,
-        /// if the <see cref="NetworkTransform"/> is handling interpolation,
-        /// set interpolation to none on the <see cref="RigidBody"/>
-        /// <br/>
-        /// Turn off physics for the rigid body until spawned, otherwise
-        /// clients can run fixed update before the first
-        /// full <see cref="NetworkTransform"/> update
-        /// </summary>
-        private void SetupRigidBody()
-        {
-            _rigidbody             = GetComponent<Rigidbody>();
-            _originalInterpolation = _rigidbody.interpolation;
-            _originalIsKinematic   = _rigidbody.isKinematic;
-
-            _rigidbody.interpolation = _isAuthority ? _originalInterpolation : (_networkTransform.Interpolate ? RigidbodyInterpolation.None : _originalInterpolation);
-            _rigidbody.isKinematic = true;
-        }
 
         /// <summary>
         /// For owner authoritative (i.e. ClientNetworkTransform)
@@ -57,6 +35,7 @@ namespace FireworksMania.Core.Common
         public override void OnGainedOwnership()
         {
             UpdateOwnershipAuthority();
+            base.OnGainedOwnership();
         }
 
         /// <summary>
@@ -66,6 +45,7 @@ namespace FireworksMania.Core.Common
         public override void OnLostOwnership()
         {
             UpdateOwnershipAuthority();
+            base.OnLostOwnership();
         }
 
         /// <summary>
@@ -74,7 +54,7 @@ namespace FireworksMania.Core.Common
         /// </summary>
         private void UpdateOwnershipAuthority()
         {
-            if (_isServerAuthoritative)
+            if (_networkTransform.IsServerAuthoritative())
             {
                 _isAuthority = NetworkManager.IsServer;
             }
@@ -84,28 +64,31 @@ namespace FireworksMania.Core.Common
             }
 
             // If you have authority then you are not kinematic
-            _rigidbody.isKinematic = _isAuthority ? _rigidbody.isKinematic : true;
+            this.Rigidbody.isKinematic = _isAuthority ? Rigidbody.isKinematic : true;
 
             // Set interpolation of the Rigidbody based on authority
             // With authority: let local transform handle interpolation
             // Without authority: let the NetworkTransform handle interpolation
-            _rigidbody.interpolation = _isAuthority ? _originalInterpolation : RigidbodyInterpolation.None;
+            this.Rigidbody.interpolation = _isAuthority ? _originalInterpolation : RigidbodyInterpolation.None;
         }
 
         /// <inheritdoc />
         public override void OnNetworkSpawn()
         {
             UpdateOwnershipAuthority();
+            base.OnNetworkSpawn();
         }
 
         /// <inheritdoc />
         public override void OnNetworkDespawn()
         {
-            _rigidbody.interpolation = _originalInterpolation;
+            this.Rigidbody.interpolation = _originalInterpolation;
             // Turn off physics for the rigid body until spawned, otherwise
             // non-owners can run fixed updates before the first full
             // NetworkTransform update and physics will be applied (i.e. gravity, etc)
-            _rigidbody.isKinematic = true;
+            this.Rigidbody.isKinematic = true;
+
+            base.OnNetworkDespawn();
         }
     }
 }
